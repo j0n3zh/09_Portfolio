b48e639317c3a43a91898cbdbb25a5605c6d0ef0
Jonas Christiansen <j0n3z@live.dk>
2016-09-26 13:37:07 +0200

    Add project files.

---
+++ 09_Portfolio/SavingsAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+namespace _09_Portfolio
+{
+    internal class SavingsAccount : Asset
+    {
+        public String Symbol { get; internal set; }
+        public double Value { get; internal set; }
+        public double InterestRate { get; internal set; }
+
+        public SavingsAccount(string symbol = "", double value = 0, double interest = 0)
+        {
+            this.Symbol = Symbol;
+            this.Value = value;
+            this.InterestRate = interest;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("SavingsAccount[value={0:#.00},interestRate={1}]", Value.ToString(CultureInfo.InvariantCulture), InterestRate);
+        }
+
+         public double GetValue()
+        {
+            return Value;
+        }
+
+        public void ApplyInterest()
+        {
+            this.Value = Value + (Value * (InterestRate/100));
+        }
+
+    }
+}
---
+++ 09_Portfolio/Stock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace _09_Portfolio
+{
+    internal class Stock : Asset
+    {
+        public double NumShares { get; internal set; }
+        public double PricePerShare { get; internal set; }
+        public string Symbol { get; internal set; }
+
+        public double GetValue()
+        {
+            return (PricePerShare * NumShares);
+        }
+
+        public Stock(string symbol = "", double pricePerShare = 0, double numShares = 0)
+        {
+            this.Symbol = symbol;
+            this.PricePerShare = pricePerShare;
+            this.NumShares = numShares;
+        }
+        public static double TotalValue(Asset[] stocks)
+        {
+            double returnValue = 0;
+            foreach (Asset element in stocks)
+            {
+                returnValue += element.GetValue();
+            }
+            return returnValue;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Stock[symbol={0},pricePerShare={1},numShares={2}]", Symbol, PricePerShare.ToString(CultureInfo.InvariantCulture), NumShares);
+        }
+
+        public bool Equals(Stock stock)
+        {
+            if (stock.NumShares == this.NumShares && stock.Symbol == this.Symbol && stock.PricePerShare == this.PricePerShare ) return true;
+            else return false; 
+        }
+    }
+}
